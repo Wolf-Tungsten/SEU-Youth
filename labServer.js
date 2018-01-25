1af1048cb59b6ab1c5ae4dd8d4d683e6ecc7783c
Wolf Tungsten <gaoruihao@outlook.com>
2018-01-25 23:07:14 +0800

    晶莹剔透亮闪闪blingbling导航栏

--- labServer.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by WolfTungsten on 2017/9/21.
- */
-var connect = require("connect");
-var serveStatic = require("serve-static");
-
-var app = connect();
-app.use(serveStatic("/Users/WolfTungsten/Development/SEU-Youth/Template"));
-app.listen(5000);
